cd442e1890fdef510393d5a653a9327499bd8cc9
simolex <simolex@yandex.ru>
2021-11-22 10:09:47 +0300

    create: add images to Slider

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  //direction: "vertical",
+  loop: true,
+  //effect: "flip",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+});
