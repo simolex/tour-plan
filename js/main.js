475f5319194d81583b82a8f3dfe00c1c3c9860d0
simolex <simolex@yandex.ru>
2021-11-24 23:12:21 +0300

    create: block Reviews

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".hotel-slider", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   //direction: "vertical",
   loop: true,
@@ -11,5 +11,21 @@ const swiper = new Swiper(".hotel-slider", {
   navigation: {
     nextEl: ".hotel-slider__button--next",
     prevEl: ".hotel-slider__button--prev",
+  },
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  //direction: "vertical",
+  loop: true,
+  //effect: "flip",
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: true,
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
 });
