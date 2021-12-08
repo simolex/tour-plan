c9cf5907e7bfef9b6fc76f70606736636d79d5b8
simolex <simolex@yandex.ru>
2021-12-08 16:00:10 +0300

    create: fake Google Map

--- js/main.js
+++ js/main.js
@@ -50,12 +50,22 @@ const unionSearchInputs = () => {
 
 unionSearchInputs();
 
-const a = `
-<iframe
-  class="map-viewport__frame"
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63281.253839198354!2d79.79499280230297!3d7.5664353377695255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2c96110de8289%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sru!2sru!4v1637658701637!5m2!1sru!2sru"
-  allowfullscreen=""
-  loading="lazy"
-  >
-</iframe>
-`;
+const activateFakeMap = () => {
+  const mapViewport = document.querySelector(".map-viewport");
+  const startMap = () => {
+    mapViewport.innerHTML = `
+      <iframe
+        class="map-viewport__frame"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63281.253839198354!2d79.79499280230297!3d7.5664353377695255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2c96110de8289%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sru!2sru!4v1637658701637!5m2!1sru!2sru"
+        allowfullscreen=""
+        loading="lazy"
+        >
+      </iframe>
+      `;
+    mapViewport.removeEventListener("click", startMap);
+  };
+
+  mapViewport.addEventListener("click", startMap);
+};
+
+activateFakeMap();
